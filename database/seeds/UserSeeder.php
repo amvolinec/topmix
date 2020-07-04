12c6e599723cd278655b96cdf145677e2173e288
medumi <medumi@icloud.com>
2020-07-04 17:58:05 +0300

    User seeder update

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,6 +20,4 @@ class UserSeeder extends Seeder
             'role_id' => 1
         ]);
     }
-
-
 }
