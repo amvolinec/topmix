e46dd1ffc61b6c39cf3a799fc14886fd2a64a93b
iryna <irynasluchova@gmail.com>
2020-07-17 07:34:46 +0300

    Lessons added

--- app/Course.php
+++ app/Course.php
@@ -9,6 +9,6 @@ class Course extends Model
     protected $fillable = ['name', 'description', 'author_id', 'published'];
 
     public function author(){
-        return $this->belongsTo('\App\User');
+        return $this->belongsTo('\App\User', 'author_id', 'id');
     }
 }
