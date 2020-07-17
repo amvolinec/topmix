9ca5c6582cb6c082854dc49d5b7d81cd5ea3ac1e
iryna <irynasluchova@gmail.com>
2020-07-17 10:57:34 +0300

    Lessons changed

--- resources/views/lesson/index.blade.php
+++ resources/views/lesson/index.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-12">
                 <h1>
-                    Lesson
+                    {{__ ('Lesson')}}
                 </h1>
                 <form action="{{ route('lesson.create') }}">
                     <button class="btn btn-success">+</button>
@@ -15,12 +15,12 @@
                         <thead class="thead-dark">
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Notes</th>
-                            <th scope="col">Select course</th>
-                            <th scope="col">Select file</th>
-                            <th scope="col">Action</th>
+                            <th scope="col">{{__ ('Title')}}</th>
+                            <th scope="col">{{__ ('Description')}}</th>
+                            <th scope="col">{{__ ('Notes')}}</th>
+                            <th scope="col">{{__ ('Select lesson')}}</th>
+                            <th scope="col">{{__('Select file')}}</th>
+                            <th scope="col">{{__ ('Action')}}</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -34,12 +34,12 @@
                                 <td>{{ $lesson->file }}</td>
                                 <td>
                                     <a class="btn btn-sm btn-success float-right" style="margin: 0 8px;"
-                                       href="{{ route('lesson.edit', $lesson->id) }}">Edit</a>
+                                       href="{{ route('lesson.edit', $lesson->id) }}">{{__ ('Edit')}}</a>
                                     <form class="float-right" action="{{ route('lesson.destroy', $lesson->id) }}"
                                           method="post" onsubmit="return confirm('Do you really want to delete?');">
                                         @method('delete')
                                         @csrf
-                                        <button class="btn btn-sm btn-danger" type="submit">Delete</button>
+                                        <button class="btn btn-sm btn-danger" type="submit">{{__ ('Delete')}}</button>
                                     </form>
                                 </td>
                             </tr>
