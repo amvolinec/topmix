231c383b4f848ee242aa8a4194f1e7f23086fdfe
medumi <medumi@icloud.com>
2020-07-23 18:21:12 +0300

    Changed to public_html

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.config.publicPath = 'public_html';
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public_html/js')
+    .sass('resources/sass/app.scss', 'public_html/css');
